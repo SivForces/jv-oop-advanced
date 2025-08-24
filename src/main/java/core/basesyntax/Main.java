8ccc03678dc7a1d9006d0ba11e3965506f4c860c
Vitalii Levda <vitalii.levda@tul.cz>
2025-08-24 11:19:03 +0300

    Main class set upped

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,20 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        final int NUMBER_OF_FIGURES = 3;
+        final int NUMBER_OF_FIGURES = 6;
         Figure[] figures = new Figure[NUMBER_OF_FIGURES];
+        FigureSupplier figureSupplier = new FigureSupplier();
+
+        for (int i = 0; i < NUMBER_OF_FIGURES / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (int i = NUMBER_OF_FIGURES / 2; i < NUMBER_OF_FIGURES; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+
+        for (Figure figure : figures) {
+            figure.draw();
+        }
     }
 }
