f8def1cc9e3deb65484fce8cd10f4c12375485b1
Vitalii Levda <vitalii.levda@tul.cz>
2025-08-24 12:00:22 +0300

    minor formatting changes

--- src/main/java/core/basesyntax/Circle.java
+++ src/main/java/core/basesyntax/Circle.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Circle extends Figure {
     private final double radius;
 
-    Circle(String color, double radius) {
+    public Circle(String color, double radius) {
         super(color);
         this.radius = radius;
     }
